2054bac7aecf4cb3cac3a62b9fd0e236858b6d3d
vraj patel <vraj.patel@primedemo.in>
2024-12-10 12:09:59 +0530

    Chnage url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    // baseURL: "http://localhost:3000/api/v1", // localhost
+    baseURL: "docwithaccesscodeback-production.up.railway.app",
+
 });
 
 axiosInstance.interceptors.response.use(
