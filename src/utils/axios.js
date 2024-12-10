2fb90674fbef297417b53d4f7c8233af44fa146f
vraj patel <vraj.patel@primedemo.in>
2024-12-10 12:14:42 +0530

    Change url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:3000/api/v1", // localhost
-    baseURL: "docwithaccesscodeback-production.up.railway.app",
+    baseURL: "docwithaccesscodeback-production.up.railway.app/api/v1",
 
 });
 
